51783d00d3bb27b9aeff9047675bab59578d89f3
Vododovoodvod <tomislav.stasina@gmail.com>
2022-05-17 21:49:19 +0200

    Added log in skeleton

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,16 @@
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { ProfilePictureSelect } from "../components/ProfilePictureSelect";
 import "../styles/Login.css"
+import { NameInput } from "../components/NameInput";
+import { SelectPicture } from "../components/SelectPicture";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 export function Login() {
-    const navigate = useNavigate();
-    const [userName, setUserName] = useState("");
-    const [userPicture, setUserPicture] = useState(null);
+    const appContext = useContext(AppContext);
     
-    const setName = (event) => {
-        const name = event.target.value;
-        setUserName(name);
-    }
-
-    const Login = (event) => {
-        event.preventDefault();
-        console.log(userName + ", " + userPicture);
-    }
-
     return(
         <div>
-            <form onSubmit={Login}>
-                <input type="text" placeholder="Your name" onChange={setName}/>
-                <span>Select picture</span>
-                <div>
-                    <ProfilePictureSelect value={0}/>
-                    <ProfilePictureSelect value={1}/>
-                    <ProfilePictureSelect value={2}/>
-                    <ProfilePictureSelect value={3}/>
-                    <ProfilePictureSelect value={4}/>
-                    <ProfilePictureSelect value={5}/>
-                    <ProfilePictureSelect value={6}/>
-                    <ProfilePictureSelect value={7}/>
-                    <ProfilePictureSelect value={8}/>
-                    <ProfilePictureSelect value={9}/>
-                </div>
-                <button type="submit">Log in</button>
-            </form>
+            {appContext.hasUserName() ? null : <NameInput/>}
+            {appContext.hasUserName() ? <SelectPicture/> : null}
         </div>
     );
 }
